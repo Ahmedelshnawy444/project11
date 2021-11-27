ad71a436873300f48c367888b3e0c498ed3ddb95
medo <medo@DESKTOP-BQD8L4C>
2021-11-27 02:05:33 +0200

    Add project files.

---
+++ Project2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> playersList = new LinkedList<string>();
+
+            LinkedListNode<string> playersNode = playersList.AddFirst("spiderman3");
+            playerslist.AddFirst("cristianoRonaldo");
+            playerslist.AddFirst("mosalah");
+            playerslist.AddBefore(playersNode, "messi");
+            playerslist.AddLast("neymar");
+
+            Console.WriteLine(" Before Deletion:  ");
+
+            foreach (var item in playersList)
+            {
+                Console.Write(item + ",");
+            }
+
+            playersList.RemoveFirst();
+            playersist.RemoveLast();
+            playersList.Remove("mosalah");
+            playersList.Remove("messi");
+
+            Console.WriteLine();
+            Console.WriteLine(" After Deletion: ");
+
+            foreach (var item in playersList)
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ project3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> cuntriesList = new LinkedList<string>();
+
+            LinkedListNode<string> countriesNode = countriesList.AddFirst("spain");
+            countriesList.AddFirst("egypt");
+            countriesList.AddFirst("brazil");
+            countriesList.AddBefore(countriesNode, "sudan");
+            countriesList.AddLast("south africa");
+
+            Console.WriteLine(" Before Reversig:  ");
+
+            foreach (var item in countriesList)
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(" List After Reversing: ");
+
+            foreach (var item in countriesList.Reverse())
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
